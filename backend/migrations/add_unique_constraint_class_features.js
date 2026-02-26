80821df0270c42625441b2a76f9a237d1405ca5c
Carl Sebastian Marnewick <bubblegumwarior@gmail.com>
2026-02-26 12:31:14 +0200

    Railway deply attempt 5

--- backend/migrations/add_unique_constraint_class_features.js
+++ backend/migrations/add_unique_constraint_class_features.js
@@ -1,11 +1,4 @@
-const { Pool } = require('pg');
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'dungeonlair',
-  password: 'admin',
-  port: 5432,
-});
+const { pool } = require('../models/database');
 
 async function addUniqueConstraintToClassFeatures() {
   const client = await pool.connect();
